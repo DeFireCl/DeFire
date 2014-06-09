4b1201552b5f1089bfd59fd1f47bbff5ce43e4f1
Matías Muñoz Espinoza <writkas2@gmail.com>
2014-06-09 19:44:01 -0400

    navegación

--- app/views/header.blade.php
+++ app/views/header.blade.php
@@ -1,6 +1,14 @@
 <header><meta http-equiv="Content-Type" content="text/html; charset=gb18030">
+    	<nav class="nav">
+    		<ul>
+    			<li><a class="nav-link" href="">Inicio</a></li>
+    			<li><a class="nav-link" href="">¿Quienes somos?</a></li>
+    			<li><a class="nav-link" href="">Portafolio</a></li>
+    			<li><a class="nav-link" href="">Contacto</a></li>
+    		</ul>
+    	</nav>
     	<div class="logo"></div>
-
+		
     	<article class="container">
 			<div class="jumbotron">
 				<h1>¡ Bienvenidos !</h1>

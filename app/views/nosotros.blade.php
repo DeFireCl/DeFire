1124de1079170a5a26dc217afe1ffc2c373e44f6
Matías Muñoz Espinoza <writkas2@gmail.com>
2014-06-12 18:52:16 -0400

    pequeños cambios en contacto y en menú

--- app/views/nosotros.blade.php
+++ app/views/nosotros.blade.php
@@ -8,24 +8,26 @@
 	
 	@include ('include/menu')
 
-	<ul class="rp-grid">
-	    <li>
-	    <div class="rp-item rp-img-1">
-	      <div class="rp-info">
-	        <h3>SEBASTIAN SILVA</h3>
-	        <p>CHIEF CREATIVE OFFICER<a href="">Follow on Twitter</a></p>
-	      </div>
-	    </div>
-	  </li>
-	  <li>
-	    <div class="rp-item rp-img-2">
-	      <div class="rp-info">
-	        <h3>MATÍAS MUÑOZ</h3>
-	        <p>CHIEF CREATIVE OFFICER<a href="">Follow on Twitter</a></p>
-	      </div>
-	    </div>
-	  </li>
-	</ul>
+	<div class="nosotros">
+		<ul class="rp-grid">
+		    <li>
+		    	<div class="rp-item rp-img-1">
+			      	<div class="rp-info">
+				        <h3>SEBASTIAN SILVA</h3>
+				        <p>PROGRAMADOR Y DISEÑADOR WEB<a href="">Siguelo en Twitter</a></p>
+			      	</div>
+		    	</div>
+		  	</li>
+		  	<li>
+		  		<div class="rp-item rp-img-2">
+		      		<div class="rp-info">
+		        		<h3>MATÍAS MUÑOZ</h3>
+		        		<p>PROGRAMADOR Y DISEÑADOR WEB<a href="">Siguelo en Twitter</a></p>
+		      		</div>
+		    	</div>
+		  	</li>
+		</ul>
+	</div>
 
 <!-- fin contenido-->
 @stop
